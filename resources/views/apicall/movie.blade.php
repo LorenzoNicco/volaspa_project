925aa6b8389538bd32cae53e398c1a945e7747b1
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-12-18 15:59:53 +0100

    fix cols in pagina movie by id

--- resources/views/apicall/movie.blade.php
+++ resources/views/apicall/movie.blade.php
@@ -4,7 +4,7 @@
     <div class="pt-12">
         <!-- Hero con le info sul film -->
         <div class="hero rounded-lg w-10/12 bg-base-200 mx-auto">
-            <div class="hero-content flex-col lg:flex-row flex-wrap grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+            <div class="hero-content flex-col lg:flex-row">
                 @if ($movie['Response'] == 'False')
                     {{-- Se la ricerca non ha avuto successo comparirà questo messaggio --}}
                     <div class="text-center">
@@ -15,54 +15,56 @@
                         <a href="{{ route('dashboard') }}" class="btn btn-primary mt-10">Torna alla Dashboard</a>
                     </div>
                 @elseif ($movie['Response'] == 'True' && $flag == true)
-                    {{-- Se la ricerca ha avuto successo e si è cercato per titolo, verranno mostrati i film in lista --}}
-                    @foreach ($movie['Search'] as $singleMovie)
-                        <div class="bg-base-100 shadow-xl">
-                            <!-- Locandina del film -->
-                            <figure>
-                                <img src="{{ $singleMovie['Poster'] }}" alt="Immagine non trovata" />
-                            </figure>
+                    <div class="flex flex-col lg:flex-row flex-wrap grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+                        {{-- Se la ricerca ha avuto successo e si è cercato per titolo, verranno mostrati i film in lista --}}
+                        @foreach ($movie['Search'] as $singleMovie)
+                            <div class="bg-base-100 shadow-xl">
+                                <!-- Locandina del film -->
+                                <figure>
+                                    <img src="{{ $singleMovie['Poster'] }}" alt="Immagine non trovata" />
+                                </figure>
 
-                            <div class="card-body">
-                                <!-- Titolo del film -->
-                                <h2 class="card-title">{{ $singleMovie['Title'] }}</h2>
+                                <div class="card-body">
+                                    <!-- Titolo del film -->
+                                    <h2 class="card-title">{{ $singleMovie['Title'] }}</h2>
 
-                                <div class="card-actions justify-end">
-                                    <button class="btn btn-primary" onclick="my_modal_{{ $singleMovie['imdbID'] }}.showModal()">Info</button>
+                                    <div class="card-actions justify-end">
+                                        <button class="btn btn-primary" onclick="my_modal_{{ $singleMovie['imdbID'] }}.showModal()">Info</button>
 
-                                    <!-- Modale per mostrare i dati dei film -->
-                                    <dialog id="my_modal_{{ $singleMovie['imdbID'] }}" class="modal">
-                                        <div class="modal-box">
-                                            <h3 class="font-bold text-lg">{{ $singleMovie['Title'] }}</h3>
+                                        <!-- Modale per mostrare i dati dei film -->
+                                        <dialog id="my_modal_{{ $singleMovie['imdbID'] }}" class="modal">
+                                            <div class="modal-box">
+                                                <h3 class="font-bold text-lg">{{ $singleMovie['Title'] }}</h3>
 
-                                            <ul>
-                                                <!-- Anno -->
-                                                <li class="flex">
-                                                    <h3 class="font-bold me-2">Anno di uscita:</h3>
-                    
-                                                    <p>{{ $singleMovie['Year'] }}</p>
-                                                </li>
-                    
-                                                <!-- Imdb ID -->
-                                                <li class="flex">
-                                                    <h3 class="font-bold me-2">Imdb ID:</h3>
-                    
-                                                    <p>{{ $singleMovie['imdbID'] }}</p>
-                                                </li>
-                                            </ul>
+                                                <ul>
+                                                    <!-- Anno -->
+                                                    <li class="flex">
+                                                        <h3 class="font-bold me-2">Anno di uscita:</h3>
+                        
+                                                        <p>{{ $singleMovie['Year'] }}</p>
+                                                    </li>
+                        
+                                                    <!-- Imdb ID -->
+                                                    <li class="flex">
+                                                        <h3 class="font-bold me-2">Imdb ID:</h3>
+                        
+                                                        <p>{{ $singleMovie['imdbID'] }}</p>
+                                                    </li>
+                                                </ul>
 
-                                            <!-- Pulsante di chiusura modale -->
-                                            <div class="modal-action">
-                                                <form method="dialog">
-                                                    <button class="btn btn-warning">Chiudi</button>
-                                                </form>
+                                                <!-- Pulsante di chiusura modale -->
+                                                <div class="modal-action">
+                                                    <form method="dialog">
+                                                        <button class="btn btn-warning">Chiudi</button>
+                                                    </form>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </dialog>
+                                        </dialog>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach                        
+                    </div>
                 @elseif ($movie['Response'] == 'True' && $flag == false)
                     {{-- Se la ricerca ha avuto successo e si è cercato per id, verranno mostrati i dati del film --}}
                     <!-- Locandina -->
